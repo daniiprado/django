48f54f4d1cb63785f381193ab9b96b584004366a
Daniel Prado <danii.prado@outlook.com.ar>
2018-06-28 13:43:23 -0500

    Configuración WebPack

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,107 +1,173 @@
-let path = require('path')
-let webpack = require('webpack')
-const { VueLoaderPlugin } = require('vue-loader')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== 'production'
+/*********************************
+ * Environment and imports
+ *********************************/
+const env = process.env.NODE_ENV || 'development';
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  mode: 'production',
-  // entry: './static/js/app.js',
-  entry: {
-    'app': [
-      './resources/assets/js/app.js',
-      './resources/assets/sass/app.scss',
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, './public'),
-    publicPath: '/public/',
-    filename: 'scripts.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-           {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-                // you can specify a publicPath here
-                // by default it use publicPath in webpackOptions.output
-                publicPath: '/public/'
-              },
-           },
-          'css-loader',
-          'sass-loader',
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        loader: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
-  },
-  resolve: {
-    modules: [
-      "node_modules"
-    ],
-    alias: {
-      vue: 'vue/dist/vue.js'
-    }
-  },
-  plugins: [
-    // make sure to include the plugin for the magic
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "styles.bundle.css",
-      chunkFilename: "[id].css"
-    })
-  ],
-  performance: {
-    hints: false
-  },
+console.log(process.env.NODE_ENV);
+console.log(env);
 
-  devtool: '#eval-source-map'
+/***************
+ * Entry
+ ***************/
+
+const entry = [
+    "./resources/assets/sass/app.scss",
+    "./resources/assets/js/app.js",
+    "./resources/assets/css/styles.css"
+];
+
+/***************
+ * Output
+ ***************/
+
+const output = {
+    filename: "[name].bundle.js",
+    chunkFilename: '[name].bundle.js',
+    publicPath: '/js/',
+    path: path.resolve(__dirname, './public/js/'),
+    pathinfo: false
+};
+
+if (env === "production") {
+    output.filename = "[name].bundle.min.js";
+    output.chunkFilename = "[name].bundle.min.js";
+    output.pathinfo = false;
 }
 
-if (process.env.NODE_ENV === 'production')
-{
-  module.exports.devtool = '#source-map'
+/***************
+ * Modules
+ ***************/
 
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
+const _module = {
+    rules: [
+        {
+            test: /\.bundle\.js$/,
+            use: 'bundle-loader'
+        },
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        },
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        },
+        {
+            test: /\.(sa|sc|c)ss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: [
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            })
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            loader: [
+                'file-loader'
+            ]
+        },
+        {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]?[hash]'
+            }
+        }
+    ]
+};
+
+/***************
+ * Optimization
+ ***************/
+
+const optimization = {
+    splitChunks: {
+        cacheGroups: {
+            styles: {
+                name: 'styles',
+                test: /\.css$/,
+                chunks: 'all',
+                enforce: true
+            },
+            common: {
+                test: /[\\/]node_modules[\\/]/,
+                name: "vendors",
+                chunks: 'all',
+                enforce: true
+            }
+        }
+    },
+    minimizer: [
+        new UglifyJsPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: true // set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
+    ]
+};
+
+
+/***************
+ * Plugins
+ ***************/
+
+const plugins = [
+    new VueLoaderPlugin(),
+    new ExtractTextPlugin({
+        filename: env === "production" ? "../css/[name].bundle.min.css" : "../css/[name].bundle.css",
+        disable: false,
+        allChunks: true
     }),
     new webpack.LoaderOptionsPlugin({
-      minimize: true
+        options: {
+            postcss: [
+                autoprefixer()
+            ]
+        },
+        minimize: env === "production",
+    }),
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        }
     })
-  ])
-}
+];
+/***************
+ * Resolve
+ ***************/
+
+const resolve = {
+    extensions: [".ts", ".js"],
+    modules: [
+        "node_modules"
+    ],
+    alias: {
+        vue: 'vue/dist/vue.js'
+    }
+};
+
+/***************
+ * Exports
+ ***************/
+
+module.exports = {
+    entry: entry,
+    output: output,
+    resolve: resolve,
+    mode: env,
+    module: _module,
+    optimization: optimization,
+    plugins: plugins
+};
